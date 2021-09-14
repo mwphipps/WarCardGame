831516c4ab6346a418684218f7036ef6892b6b22
Michael Phipps <michael@giantsource.com>
2021-09-14 01:00:55 -0400

    Initial tutorial app done. Card war game now works as expected when compiled.

--- WarCardGame/ViewController.swift
+++ WarCardGame/ViewController.swift
@@ -8,16 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
-
     
     @IBOutlet weak var leftImageView: UIImageView!
-    
+
     @IBOutlet weak var rightImageView: UIImageView!
     
-    
     @IBOutlet weak var leftScoreLabel: UILabel!
-    
+
     @IBOutlet weak var rightScoreLabel: UILabel!
+    
+    var leftScore = 0
+    var rightScore = 0
     
     
     override func viewDidLoad() {
@@ -28,18 +29,32 @@ class ViewController: UIViewController {
     
     @IBAction func dealTapped(_ sender: Any) {
         
+        // Randomize numbers
         let leftNumber = Int.random(in: 2...14)
-        // print(leftNumber)
-        
         let rightNumber = Int.random(in: 2...14)
-        // print(rightNumber)
         
+        // Update Image views
         leftImageView.image = UIImage(named: "card\(leftNumber)")
-        
         rightImageView.image = UIImage(named: "card\(rightNumber)")
         
-    }
+        // Compare the numbers
+        if leftNumber > rightNumber {
     
-
+            // Left side wins
+            leftScore += 1
+            
+            leftScoreLabel.text = String(leftScore)
+        }
+        else if leftNumber < rightNumber {
+    
+            // Right side wins
+            rightScore += 1
+            
+            rightScoreLabel.text = String(rightScore)
+        }
+        else {
+            // Tie ?!
+        }
+    }
 }
 
